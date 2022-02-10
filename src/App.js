7b440192c453d376fe4a1036ebcbca820b60c8f4
rissba05 <rissba05@gmail.com>
2022-02-10 13:04:25 -0600

    demonstrating react fragment <></>
    <></>

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 const App = () => {
   return (
-    <h1>
-      Hello React !
-    </h1>
+    <>
+      <h1>
+        Hello React !
+      </h1>
+      <h1>
+        Hello Larry !
+      </h1>
+    </>
   );
 }
 
