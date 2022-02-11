099790317802a287ee7325c58054c001ab0e3c63
rissba05 <rissba05@gmail.com>
2022-02-11 11:59:44 -0600

    destructuring props at the top..
    more on destructuring next

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,25 +1,29 @@
-const Greeting = (props) => {
+const Greeting = ({
+    name
+    , favoriteNumber
+    , favoriteColors
+}) => {
 
-    console.log(props);
+    //console.log(props);
 
     return (
     <>    
         <h2>
-            Hello {props.name} !
+            Hello {name} !
         </h2>
         <h3>
-            {props.name}'s favorite number is {props.favoriteNumber}
+            {name}'s favorite number is {favoriteNumber}
         </h3>
         <h3>
-            {props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}
+            {name}'s favorite number plus 10 is {favoriteNumber + 10}
         </h3>
         <h3>
-            {props.name}'s favorite colors are:
+            {name}'s favorite colors are:
         </h3>
         <ul>
             {
-                props.favoriteColors 
-                && [...new Set(props.favoriteColors)].map(x => (
+                favoriteColors 
+                && [...new Set(favoriteColors)].map(x => (
                     <li
                         key={x}
                     >
@@ -27,7 +31,7 @@ const Greeting = (props) => {
                     </li>
                 ))
             }
-            {!props.favoriteColors && <li>None</li>}
+            {!favoriteColors && <li>None</li>}
         </ul>
     </>
     );
