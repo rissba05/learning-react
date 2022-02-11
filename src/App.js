267fa8d7d2e44571cf2e856008d28b50601ef0bc
rissba05 <rissba05@gmail.com>
2022-02-11 11:43:28 -0600

    passed favoriteColors prop, saw using map to stamp out list items
    and boolean short circuit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
       <Greeting 
         name='Curly'
         favoriteNumber={42}
+        favoriteColors={["Red", "Green", "Blue"]}
       />
       <Greeting 
         name='Moe'
--- src/Greeting.js
+++ src/Greeting.js
@@ -13,6 +13,13 @@ const Greeting = (props) => {
         <h3>
             {props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}
         </h3>
+        <h3>
+            {props.name}'s favorite colors are:
+        </h3>
+        <ul>
+            {props.favoriteColors && props.favoriteColors.map(x => <li>{x}</li>)}
+            {!props.favoriteColors && <li>None</li>}
+        </ul>
     </>
     );
   };
